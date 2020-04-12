4763184083d5a25b848ed8e9bd15d7e9f259b439
mosh <mosh@itu.dk>
2020-04-12 14:39:05 +0200

    changed pool values

--- app/helpers/common.go
+++ app/helpers/common.go
@@ -42,9 +42,9 @@ func InitDB() *gorm.DB {
 
 	db, err = gorm.Open("postgres", connString)
 
-	db.DB().SetMaxIdleConns(50)
-	db.DB().SetMaxOpenConns(500)
-	db.DB().SetConnMaxLifetime(time.Hour)
+	db.DB().SetMaxIdleConns(25)
+	db.DB().SetMaxOpenConns(25)
+	db.DB().SetConnMaxLifetime(5*time.Minute)
 
 
 
